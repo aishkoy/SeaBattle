0285c8654f3f2d083745bc60aaa3a085752cfff0
Aisha <aisha.orozbekova@alatoo.edu.kg>
2024-10-20 21:58:23 +0600

    Set up the playing field and the number of different ships

---
+++ src/SeaBattle.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class SeaBattle {
+
+    public static void main(String[] args) {
+        int rows = 7;
+        int cols = 7;
+        int[][] matrix = new int[rows][cols];
+        Random random = new Random();
+
+        int[][] blockSizes = {{1, 3}, {1, 2}, {1, 2}, {1, 1}, {1, 1}, {1, 1}, {1, 1}};
+    }
+}
