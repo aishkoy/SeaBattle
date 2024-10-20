3eacf6a8699b678e3caf8aa27ce5af610e99bad5
Aisha <aisha.orozbekova@alatoo.edu.kg>
2024-10-20 22:03:00 +0600

    added a method to check if a ship can be placed

--- src/SeaBattle.java
+++ src/SeaBattle.java
@@ -10,4 +10,35 @@ public class SeaBattle {
 
         int[][] blockSizes = {{1, 3}, {1, 2}, {1, 2}, {1, 1}, {1, 1}, {1, 1}, {1, 1}};
     }
+
+
+    public static boolean canPlaceBlock(int[][] matrix, int row, int col, int blockHeight, int blockWidth, boolean isHorizontal) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        if (isHorizontal) {
+            if (col + blockWidth > cols || row + blockHeight > rows) {
+                return false;
+            }
+            for (int i = row - 1; i <= row + blockHeight; i++) {
+                for (int j = col - 1; j <= col + blockWidth; j++) {
+                    if (i >= 0 && i < rows && j >= 0 && j < cols && matrix[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        } else {
+            if (row + blockHeight > rows || col + blockWidth > cols) {
+                return false;
+            }
+            for (int i = row - 1; i <= row + blockHeight; i++) {
+                for (int j = col - 1; j <= col + blockWidth; j++) {
+                    if (i >= 0 && i < rows && j >= 0 && j < cols && matrix[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
